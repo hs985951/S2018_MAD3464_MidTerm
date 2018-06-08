2711a14b51b2b898396b1716f50468464b63351c
Priteshkumar Patel <pritesh.patel@Tor.Loc>
2018-06-07 20:31:50 -0400

    project

--- C0736042_midterm_mads3464/src/c0736042_midterm_mads3464/C0736042_midterm_mads3464.java
+++ C0736042_midterm_mads3464/src/c0736042_midterm_mads3464/C0736042_midterm_mads3464.java
@@ -22,14 +22,22 @@ public class C0736042_midterm_mads3464 {
         for (int i = try1.length-1; i>=0; i--)
             System.out.print(try1[i]);
         
-        String s1="1101";  
+    
+    
+        String s1="1111";  
 int decimal=Integer.parseInt(s1,2);  
  System.out.println(decimal);
-    }
     
    
+   
+       String s3 = new String("the dog jumped over the fence");
 
-     
+       System.out.print("String after replacing the with that :" );
+       System.out.println(s3.replaceFirst("the", "that"));
+
+       System.out.print("String after replacing Site name:" );
+       System.out.println(s3.replaceFirst("the", "this"));
+    }
              
 }
     
---
+++ C0736042_midterm_mads3464/src/c0736042_midterm_mads3464/stringreplace.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0736042_midterm_mads3464;
+
+/**
+ *
+ * @author pritesh.patel
+ */
+public class stringreplace {
+    String s = "i am a laegend";
+    system.out.println(s.r)
+    
+}
